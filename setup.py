84c2cd98db0a2fe5ac0cc301bb1e4b5f9e61be1a
Injng <jlinjiang@gmail.com>
2023-04-14 21:11:15 -0700

    PyPi

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+        name="zhcode",
+        version="0.0.1",
+        author="lnjng",
+        description="Programming languages in Chinese.",
+        packages=["python"]
+        )
